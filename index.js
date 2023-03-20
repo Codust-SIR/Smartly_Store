1f181e1c576c1c8d7d1f0cb1e0e73337139b97fd
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-03-20 16:17:08 +0300

    Changed the then syntax to async/awit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // fetch data from a server
-function getProducts() {
-  return fetch("http://localhost:3001/products").then((res) =>
-    res.json().then((data) => data)
-  );
+
+async function getProducts() {
+  const res = await fetch("http://localhost:3001/products");
+  const data = await res.json();
+  return data;
 }
+
 async function init() {
   const products = await getProducts();
   console.log(products);
