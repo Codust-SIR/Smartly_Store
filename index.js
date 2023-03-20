746bd44f0a7a97786f6ab6417d02d174c5a3d5d7
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-03-20 17:11:40 +0300

    Fixed

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-// fetch data from a server
-
-async function getProducts() {
-  const res = await fetch("http://localhost:3001/products");
-  const data = await res.json();
-  return data;
+async function getProductsFromServer () {
+  const response = await fetch('http://localhost:3001/products')
+  console.log("response", response)
 }
 
-async function init() {
-  const products = await getProducts();
-  console.log(products);
+async function initApp () {
+  await getProductsFromServer()
 }
 
-init();
+initApp()
