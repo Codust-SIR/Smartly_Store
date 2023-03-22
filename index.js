a2d47ba23e4057738ca99f8dbe3e63dcddde2311
charzlwebz <charzlwebz256@gmail.com>
2023-03-22 16:39:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// fetch data from a server
+/* fetch data from a server
 function getProducts() {
   return fetch("http://localhost:3001/products").then((res) =>
     res.json().then((data) => data)
@@ -10,3 +10,16 @@ async function init() {
 }
 
 init();
+*/
+
+
+async function getusers(){
+  // Fetching users from the json placeholder API
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  // Looping through the users array
+  users.map((user) =>{
+    console.log(user.name)
+  })
+}
+getusers();
