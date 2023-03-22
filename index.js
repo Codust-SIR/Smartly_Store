4016a95af6d6a2a5c56a7124bbd6585ddde14ab4
charzlwebz <charzlwebz256@gmail.com>
2023-03-22 16:51:35 +0300

    Changed

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
+async function getPostsFromTypicode() {
+  let response = await fetch("https://jsonplaceholder.typicode.com/users");
+  let users = await response.json();
+  console.log("users :>>", users);
+  users.map((user)=>{
+    document.getElementById("reviewers").innerHTML +=`
+    <div id="user">
+      <div id="user_top">
+      <div id="user_avata"></div>
+      <div id="user_names">
+        <h4>${user.name}</h4>
+        <p>${user.usernames}</p>
+      </div>
+    </div>
+    <div id="user_bottom">
+      <p>company:${user.company.name}</p>
+      <p>city:${user.address.city}</p>
+      <p>Email:${user.email}</p>
+      <a href="${user.website}">$user.website}</a>
+    </div>
+</div>
+`
+  })
+  //filter method to filter users whit the name include "p"
+  let usersNameP = users.filter((users) => {
+    return users.name.includes("P");
+  });
 /* fetch data from a server
 function getProducts() {
   return fetch("http://localhost:3001/products").then((res) =>
     res.json().then((data) => data)
   );
 }
-async function init() {
-  const products = await getProducts();
-  console.log(products);
+
+async function initApp () {
+  await getPostsFromTypicode()
 }
 
 init();
-*/
-
-
-async function getusers(){
-  // Fetching users from the json placeholder API
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
-  // Looping through the users array
-  users.map((user) =>{
-    console.log(user.name)
-  })
-}
-getusers();
