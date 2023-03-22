6dde1bbe8898119a5e3952134b2c6f401b78e7ee
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-03-22 15:35:04 +0300

    Arrenged the codes

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ async function getPostsFromTypicode() {
   let users = await response.json();
   console.log("users :>>", users);
   users.map((user)=>{
-document.getElementById("reviewers").innerHTML +=`
-<div id="user">
-<div id="user_top">
-<div id="user_avata"></div>
-<div id="user_names">
-<h4>${user.name}</h4>
-<p>${user.usernames}</p>
-</div>
-</div>
-<div id="user_bottom">
-<p>company:${user.company.name}</p>
-<p>city:${user.address.city}</p>
-<p>Email:${user.email}</p>
-<a href="${user.website}">$user.website}</a>
-</div>
+    document.getElementById("reviewers").innerHTML +=`
+    <div id="user">
+      <div id="user_top">
+      <div id="user_avata"></div>
+      <div id="user_names">
+        <h4>${user.name}</h4>
+        <p>${user.usernames}</p>
+      </div>
+    </div>
+    <div id="user_bottom">
+      <p>company:${user.company.name}</p>
+      <p>city:${user.address.city}</p>
+      <p>Email:${user.email}</p>
+      <a href="${user.website}">$user.website}</a>
+    </div>
 </div>
 `
   })
