78d0da7a44f273c2ca9617e5b6b3fa1b44b49f02
shyaka cedrick <shyakacedrick98@gmail.com>
2023-03-22 15:01:02 +0300

    added user to reviewers

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ async function getPostsFromTypicode() {
   let response = await fetch("https://jsonplaceholder.typicode.com/users");
   let users = await response.json();
   console.log("users :>>", users);
+  users.map((user)=>{
+document.getElementById("reviewers").innerHTML +=`
+<div id="user">
+<div id="user_top">
+<div id="user_avata"></div>
+<div id="user_names">
+<h4>${user.name}</h4>
+<p>${user.usernames}</p>
+</div>
+</div>
+<div id="user_bottom">
+<p>company:${user.company.name}</p>
+<p>city:${user.address.city}</p>
+<p>Email:${user.email}</p>
+<a href="${user.website}">$user.website}</a>
+</div>
+</div>
+`
+  })
   //filter method to filter users whit the name include "p"
   let usersNameP = users.filter((users) => {
     return users.name.includes("P");
@@ -10,3 +29,6 @@ async function getPostsFromTypicode() {
   console.log("users names whith P", usersNameP);
 }
 getPostsFromTypicode();
+
+
+document.getElementById("reviewers").innerHTML +=`this is the reviewers section`
