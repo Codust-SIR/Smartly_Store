5a60a2f109ba4f1309eb9980c91296078108ccfc
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-03-23 15:29:46 +0300

    Start

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-async function getPostsFromTypicode() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/users");
-  let users = await response.json();
-  console.log("users :>>", users);
-  users.map((user)=>{
-    document.getElementById("reviewers").innerHTML +=`
-    <div id="user">
-      <div id="user_top">
-      <div id="user_avata"></div>
-      <div id="user_names">
-        <h4>${user.name}</h4>
-        <p>${user.usernames}</p>
-      </div>
-    </div>
-    <div id="user_bottom">
-      <p>company:${user.company.name}</p>
-      <p>city:${user.address.city}</p>
-      <p>Email:${user.email}</p>
-      <a href="${user.website}">$user.website}</a>
-    </div>
-</div>
-`
-  })
-  //filter method to filter users whit the name include "p"
-  let usersNameP = users.filter((users) => {
-    return users.name.includes("P");
+async function getUsers() {
+  //  Fetching users from the jsonplaceholder API
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  // Looping through the users array
+  users.map((user) => {
+    document.getElementById("reviewers").innerHTML += `
+      <p>${user.email}</p>
+    `;
   });
 }
 
-async function initApp () {
-  await getPostsFromTypicode()
-}
-
-initApp()
+getUsers();
+// Array.prototype.map()
